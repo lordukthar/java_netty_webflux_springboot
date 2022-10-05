634e2eca843cd5b72fb069a514f12205dc9bce77
jonas aspemo <jonas.aspemo@sbab.se>
2022-10-05 10:38:16 +0200

    Added insert in db flyway

---
+++ src/main/resources/db/migration/V2__insert-users-data.sql
@@ -0,0 +1,17 @@
+insert into USERS values(1, 'Tony');
+insert into USERS values(2, 'John');
+insert into USERS values(3, 'Jane');
+insert into USERS values(4, 'Eric');
+insert into USERS values(5, 'Donald');
+insert into USERS values(6, 'Hanna');
+insert into USERS values(7, 'Eve');
+insert into USERS values(8, 'Amanda');
+insert into USERS values(9, 'Frank');
+insert into USERS values(10, 'Ellie');
+insert into USERS values(11, 'Aretha');
+insert into USERS values(12, 'Whitney');
+insert into USERS values(13, 'John');
+insert into USERS values(14, 'Patric');
+insert into USERS values(15, 'Lily');
+
+commit;
